e55180dbefa96863df7bfc0c653667fb962b2cdc
vtejaeta <vtejaeta@gmail.com>
2021-12-05 14:06:03 +0530

    Project init with React, TS, Taiwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-300 flex justify-center items-center">
+      <h1 className="text-2xl">My Starter of React, TypeScript, Tailwind</h1>
     </div>
   );
 }
